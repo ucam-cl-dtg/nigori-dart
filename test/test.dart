8d2a79b704755303da8956132b10264c1f0e0b46
Daniel Thomas <drt24@cam.ac.uk>
2012-12-06 17:09:45 +0000

    Add test on return type of byteconcat
    Signed-off-by: Daniel Thomas <drt24@cam.ac.uk>

--- test/test.dart
+++ test/test.dart
@@ -1,6 +1,9 @@
 import 'package:unittest/unittest.dart';
 import 'package:nigori/nigori.dart';
+import 'dart:scalarlist';
+
 
 void main() {
   test('byteconcat runs', () => byteconcat("first", "second"));
+  test('byteconcat type', () => expect(byteconcat("first", "second"),new isInstanceOf<ByteArray>()));
 }
